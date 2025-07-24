0fc6646d7f19f053be6bb3e689844c46f37fb0d1
Foysal <145685192+Foysal-001@users.noreply.github.com>
2025-07-24 20:07:50 +0600

    Update Fast_Modular_Exponentiation.py

--- TLE_slayer/Fast_Modular_Exponentiation.py
+++ TLE_slayer/Fast_Modular_Exponentiation.py
@@ -1,8 +1,13 @@
-#Fast Exponentiation (binary exponentiation)
+'''Fast Modular Exponentiation 
+This method is also known as Binray exponentiation. It is used to find the remainder of two large number powered to each other 
+like a=10^18 and b=10^12 you are asked to find a^b%(a given integer) in this case tradition method fails to compute as the number
+get unusually large. But here binary exponentiation walks in and solves it in a fraction of time because Binary exponentiation solves 
+this problem in O(log_b) time complexity!!'''
+
 import sys
 input=sys.stdin.readline
 a,b=map(int, input().split())
-mod=107
+mod=int(input())
 rem=1
 a%=mod
 while b:
